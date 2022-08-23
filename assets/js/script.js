c8b067c3765624f5c67261b2dc7a54b005f4b35f
MD-Baki <bakimd77@gmail.com>
2022-08-23 22:15:45 +0600

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,38 @@ for (const selectBtn of selectAllBtn) {
             selectPlayerList.appendChild(li);
         }
         else {
-            alert("Select Onle Five Player")
+            alert("Select Only Five Player")
         }
     })
 }
+
+function getNumberFromFilde(id) {
+    const fildeName = document.getElementById(id);
+    const stringValue = fildeName.value;
+    const convertNumber = parseInt(stringValue);
+    return convertNumber;
+}
+function setValueById(id, value) {
+    const valueId = document.getElementById(id);
+    valueId.innerText = value;
+}
+
+
+const playerCost = document.getElementById("player-cost");
+playerCost.addEventListener("click", function () {
+    const playerCounter = document.querySelectorAll("li");
+
+    const playerCostCount = getNumberFromFilde("per-player-cost");
+    const playerCostTotal = playerCostCount * playerCounter.length;
+    setValueById("player-expenses", playerCostTotal);
+})
+
+const totalCost = document.getElementById("calc-total-btn");
+totalCost.addEventListener("click", function () {
+
+    const playerTotalCost = getValueFromText("player-expenses");
+    const managerCost = getElementById("manager-cost");
+    const coachCost = getElementById("coach-cost");
+    const finalCost = playerTotalCost + managerCost + coachCost;
+    setTextById("total", finalCost);
+})
